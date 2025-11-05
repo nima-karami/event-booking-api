c254054e71ead74687e934a3e4eaf3f40d6ab6fa
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2025-11-04 21:14:40 -0500

    setup gin server

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.Run(":8080")
 }
